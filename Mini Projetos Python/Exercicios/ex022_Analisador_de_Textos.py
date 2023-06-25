f7cac63237ff5db1835b8b81a0fbd8e20d98b5bd
Felipe Cunha <felipe_cordeiro12@hotmail.com>
2023-06-25 03:36:54 -0300

    Mais exercicios

---
+++ Mini Projetos Python/Exercicios/ex022_Analisador_de_Textos.py
@@ -0,0 +1,6 @@
+nome = input('Digite o nome completo: ')
+print(nome.upper())
+print(nome.lower())
+nomeNospace = nome.replace(' ', '')
+print(len(nomeNospace))
+print(nome.split()[0])
---
+++ Mini Projetos Python/Exercicios/exE001_Frase_aleatoria.py
@@ -0,0 +1,7 @@
+from random import randrange
+frase = ["A vida é uma jornada incrível", "A persistência leva ao sucesso", "O conhecimento é poder",
+         "A imaginação não tem limites", "A gratidão transforma vidas"]
+fraseAleatoria = frase[randrange(6)]
+
+print('Pensando na sua frase .... \n')
+print(fraseAleatoria)
